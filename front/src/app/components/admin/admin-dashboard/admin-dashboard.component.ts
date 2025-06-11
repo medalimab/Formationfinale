c608dab908ce3d45297cc10a70106d7984a99f4f
dali <mabroukdali1925@gmail.com>
2025-06-11 12:28:44 +0200

    dash ajustement

--- front/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ front/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AdminUsersComponent } from '../admin-users.component';
+import { UserService } from '../../../services/user.service';
+import { FormationService } from '../../../services/formation.service';
+import { ServiceApiService } from '../../../services/service-api.service';
+import { DevisService } from '../../../services/devis.service';
+import { RendezVousService } from '../../../services/rendez-vous.service';
+import { BlogService } from '../../../services/blog.service';
+import { TemoignageService } from '../../../services/temoignage.service';
 
 interface DashboardCount {
   users?: number;
@@ -82,22 +89,39 @@ export class AdminDashboardComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private formationService: FormationService,
+    private serviceApi: ServiceApiService,
+    private devisService: DevisService,
+    private rendezVousService: RendezVousService,
+    private blogService: BlogService,
+    private temoignageService: TemoignageService
+  ) { }
 
   ngOnInit(): void {
-    // Ici, vous pourriez appeler différentes API pour obtenir les comptes
-    // pour le tableau de bord. Pour l'instant, nous simulons les données.
-    setTimeout(() => {
+    this.loading = true;
+    Promise.all([
+      this.userService.getUsers().toPromise(),
+      this.formationService.getFormations(1, 1).toPromise(),
+      this.serviceApi.getServices().toPromise(),
+      this.devisService.getDevis().toPromise(),
+      this.rendezVousService.getRendezVous().toPromise(),
+      this.blogService.getArticles().toPromise(),
+      this.temoignageService.getTemoignages().toPromise()
+    ]).then(([users, formations, services, devis, rendezVous, blogs, temoignages]) => {
       this.counts = {
-        users: 120,
-        formations: 45,
-        services: 18,
-        devis: 32,
-        rendezVous: 27,
-        blogs: 15,
-        temoignages: 24
+        users: Array.isArray(users?.data) ? users.data.length : (users?.data?.data?.length || 0),
+        formations: formations?.count || (formations?.data?.length || 0),
+        services: Array.isArray(services?.data) ? services.data.length : (services?.data?.length || 0),
+        devis: Array.isArray(devis?.data) ? devis.data.length : (devis?.data?.length || 0),
+        rendezVous: Array.isArray(rendezVous?.data) ? rendezVous.data.length : (rendezVous?.data?.length || 0),
+        blogs: Array.isArray(blogs?.data) ? blogs.data.length : (blogs?.data?.length || 0),
+        temoignages: Array.isArray(temoignages?.data) ? temoignages.data.length : (temoignages?.data?.length || 0)
       };
       this.loading = false;
-    }, 1000);
+    }).catch(() => {
+      this.loading = false;
+    });
   }
 }
